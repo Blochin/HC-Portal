1109abaf7bac0083ba3e678c9933caf17315e5bd
blochin <sevcik.lubomir02@gmail.com>
2024-01-31 15:03:15 +0100

    change api url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://diplomovka.site/",
+  baseURL: "https://api.hcportal.eu/",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
